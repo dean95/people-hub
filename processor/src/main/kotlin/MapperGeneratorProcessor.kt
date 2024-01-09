3e14f6a18f033272ea68f312fae9da4eb45ba71f
Dean Djermanovic <dean.djermanovic@five.agency>
2024-01-09 16:08:42 +0100

    MapperGeneratorProcessor cleanup

--- processor/src/main/kotlin/MapperGeneratorProcessor.kt
+++ processor/src/main/kotlin/MapperGeneratorProcessor.kt
@@ -19,57 +19,79 @@ import validator.SymbolValidator
 
 class MapperGeneratorProcessor(
     private val codeGenerator: CodeGenerator,
-    private val logger: KSPLogger
+    logger: KSPLogger
 ) : SymbolProcessor {
 
     private val symbolValidator = SymbolValidator(logger)
 
+    // Process the symbols with the GenerateMapper annotation
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        var unresolvedSymbols = emptyList<KSAnnotated>()
-        val annotationName = GenerateMapper::class.qualifiedName
+        val annotationName = GenerateMapper::class.qualifiedName ?: return emptyList()
+        val resolvedSymbols = resolver.getSymbolsWithAnnotation(annotationName).toList()
+        val validatedSymbols = resolvedSymbols.filter(symbolValidator::isValid).toList()
+        val classDeclarations = validatedSymbols.filterIsInstance<KSClassDeclaration>()
 
-        if (annotationName != null) {
-            val resolvedSymbols = resolver.getSymbolsWithAnnotation(annotationName).toList()
-            val validatedSymbols = resolvedSymbols.filter(symbolValidator::isValid).toList()
-            val classDeclarations = validatedSymbols.filterIsInstance<KSClassDeclaration>()
+        val mapperInterfaces = mutableMapOf<String, TypeSpec.Builder>()
+        val mapperImplementations = mutableMapOf<String, TypeSpec.Builder>()
 
-            val mapperInterfaces = mutableMapOf<String, TypeSpec.Builder>()
-            val mapperImplementations = mutableMapOf<String, TypeSpec.Builder>()
-
-            classDeclarations.forEach { classDeclaration ->
-                val mapperName = classDeclaration.annotations.find {
-                    it.annotationType.resolve().declaration.qualifiedName?.asString() == annotationName
-                }?.arguments?.find { it.name?.asString() == "name" }?.value.toString()
-                val mapperInterfaceBuilder = mapperInterfaces.getOrPut(mapperName) {
-                    TypeSpec.interfaceBuilder(mapperName)
-                }
-                val implClassName = "${mapperName}Impl"
-                val mapperImplementationBuilder = mapperImplementations.getOrPut(implClassName) {
-                    TypeSpec.classBuilder(implClassName)
-                        .addSuperinterface(ClassName("", mapperName))
-                        .addModifiers(KModifier.INTERNAL)
-                }
-                generateMapperMethods(resolver, classDeclaration, mapperInterfaceBuilder, mapperImplementationBuilder)
-            }
-
-            mapperInterfaces.forEach { (name, builder) ->
-                val file = FileSpec.builder("", name)
-                    .addType(builder.build())
-                    .build()
-                file.writeTo(codeGenerator, false)
-            }
-
-            mapperImplementations.forEach { (name, builder) ->
-                val file = FileSpec.builder("", name)
-                    .addType(builder.build())
-                    .build()
-                file.writeTo(codeGenerator, false)
-            }
-
-            unresolvedSymbols = resolvedSymbols - validatedSymbols
+        // Generate mappers for each class declaration
+        classDeclarations.forEach { classDeclaration ->
+            generateMappersForClass(resolver, classDeclaration, annotationName, mapperInterfaces, mapperImplementations)
         }
 
-        return unresolvedSymbols
+        // Write the generated mapper interfaces and implementations to files
+        writeGeneratedMappersToFile(mapperInterfaces, mapperImplementations)
+
+        // Return the symbols that could not be resolved
+        return resolvedSymbols - validatedSymbols
+    }
+
+    // Generate mappers for a single class declaration
+    private fun generateMappersForClass(
+        resolver: Resolver,
+        classDeclaration: KSClassDeclaration,
+        annotationName: String,
+        mapperInterfaces: MutableMap<String, TypeSpec.Builder>,
+        mapperImplementations: MutableMap<String, TypeSpec.Builder>
+    ) {
+        val mapperName = extractMapperName(classDeclaration, annotationName)
+        val mapperInterfaceBuilder = mapperInterfaces.getOrPut(mapperName) {
+            TypeSpec.interfaceBuilder(mapperName)
+        }
+        val implClassName = "${mapperName}Impl"
+        val mapperImplementationBuilder = mapperImplementations.getOrPut(implClassName) {
+            TypeSpec.classBuilder(implClassName)
+                .addSuperinterface(ClassName("", mapperName))
+                .addModifiers(KModifier.INTERNAL)
+        }
+        generateMapperMethods(resolver, classDeclaration, mapperInterfaceBuilder, mapperImplementationBuilder)
+    }
+
+    // Extract the mapper name from class annotations
+    private fun extractMapperName(classDeclaration: KSClassDeclaration, annotationName: String) = classDeclaration
+        .annotations
+        .find {
+            it.annotationType.resolve().declaration.qualifiedName?.asString() == annotationName
+        }?.arguments?.find { it.name?.asString() == "name" }?.value.toString()
+
+    // Write generated mapper interfaces and implementations to files
+    private fun writeGeneratedMappersToFile(
+        mapperInterfaces: Map<String, TypeSpec.Builder>,
+        mapperImplementations: Map<String, TypeSpec.Builder>
+    ) {
+        mapperInterfaces.forEach { (name, builder) ->
+            val file = FileSpec.builder("", name)
+                .addType(builder.build())
+                .build()
+            file.writeTo(codeGenerator, false)
+        }
+
+        mapperImplementations.forEach { (name, builder) ->
+            val file = FileSpec.builder("", name)
+                .addType(builder.build())
+                .build()
+            file.writeTo(codeGenerator, false)
+        }
     }
 
     private fun generateMapperMethods(
@@ -79,12 +101,12 @@ class MapperGeneratorProcessor(
         mapperImplementationBuilder: TypeSpec.Builder
     ) {
         val entityClassName = ClassName(classDeclaration.packageName.asString(), classDeclaration.simpleName.asString())
-
-        val domainClassName =
-            ClassName.bestGuess("peoplehub.domain.model.${entityClassName.simpleName.removeSuffix("Entity")}")
-        val domainClassDeclaration =
-            resolver.getClassDeclarationByName(resolver.getKSNameFromString(domainClassName.canonicalName))
-                ?: error("Cannot get class declaration")
+        val domainClassName = ClassName.bestGuess(
+            "peoplehub.domain.model.${entityClassName.simpleName.removeSuffix("Entity")}"
+        )
+        val domainClassDeclaration = resolver.getClassDeclarationByName(
+            resolver.getKSNameFromString(domainClassName.canonicalName)
+        ) ?: error("Cannot get class declaration")
 
         val entityProperties = classDeclaration.getDeclaredProperties().toSet()
         val entityPropertiesNames = entityProperties.map { it.simpleName.asString() }.toSet()
@@ -92,9 +114,9 @@ class MapperGeneratorProcessor(
         val domainPropertiesNames = domainProperties.map { it.simpleName.asString() }.toSet()
         val commonPropertiesNames = entityPropertiesNames.intersect(domainPropertiesNames)
 
-        val commonProperties =
-            (entityProperties + domainProperties).filter { it.simpleName.asString() in commonPropertiesNames }
-                .distinctBy { it.simpleName.asString() }
+        val commonProperties = (entityProperties + domainProperties)
+            .filter { it.simpleName.asString() in commonPropertiesNames }
+            .distinctBy { it.simpleName.asString() }
 
         // Generate methods for the interface
         val mapToDomainInterfaceFunction = FunSpec.builder(mapperFunctionName(domainClassName))
@@ -105,6 +127,7 @@ class MapperGeneratorProcessor(
             )
             .returns(domainClassName)
             .build()
+
         val mapToEntityInterfaceFunction = FunSpec.builder(mapperFunctionName(entityClassName))
             .addModifiers(KModifier.ABSTRACT)
             .addParameter(
@@ -134,6 +157,7 @@ class MapperGeneratorProcessor(
                 )
             )
             .build()
+
         val mapToEntityImplFunction = FunSpec.builder(mapperFunctionName(entityClassName))
             .addModifiers(KModifier.OVERRIDE)
             .addParameter(
@@ -149,7 +173,9 @@ class MapperGeneratorProcessor(
                 )
             )
             .build()
-        mapperImplementationBuilder.addFunction(mapToDomainImplFunction)
+
+        mapperImplementationBuilder
+            .addFunction(mapToDomainImplFunction)
             .addFunction(mapToEntityImplFunction)
     }
 
