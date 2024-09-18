7554eb00eb72aa5337a32c1892be181ca611723e
Dean Djermanovic <dean.djermanovic@five.agency>
2024-09-18 08:52:50 +0200

    Enable edge-to-edge content

--- app/src/main/kotlin/peoplehub/ui/MainActivity.kt
+++ app/src/main/kotlin/peoplehub/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package peoplehub.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +13,7 @@ import peoplehub.ui.theme.PeopleHubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             PeopleHubTheme {
